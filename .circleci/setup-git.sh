05d43ba956c56acc352500375ccb2e0ddfc35fa5
Tom Akehurst <tom@wiremock.org>
2020-03-03 15:21:36 +0000

    Added a CircleCI config

---
+++ .circleci/setup-git.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git config --global user.email "admin@mocklab.io"
+git config --global user.name "MockLab Admin"
---
+++ .circleci/setup-heroku.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
+mkdir -p /usr/local/lib /usr/local/bin
+sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
+sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
+
+cat > ~/.netrc << EOF
+machine api.heroku.com
+login $HEROKU_LOGIN
+password $HEROKU_API_KEY
+machine git.heroku.com
+login $HEROKU_LOGIN
+password $HEROKU_API_KEY
+EOF
+
+# Add heroku.com to the list of known hosts
+ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
