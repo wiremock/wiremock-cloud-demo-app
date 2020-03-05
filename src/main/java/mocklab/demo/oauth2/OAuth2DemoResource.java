746cabfd3a8b67c451db039ce7720a8794a0929b
Tom Akehurst <tom@wiremock.org>
2020-03-05 15:14:18 +0000

    Changed OAuth login demo flow to a toggle between real and mock Google login

--- src/main/java/mocklab/demo/oauth2/OAuth2DemoResource.java
+++ src/main/java/mocklab/demo/oauth2/OAuth2DemoResource.java
@@ -4,7 +4,9 @@ import com.google.common.collect.ImmutableMap;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -22,7 +24,20 @@ public class OAuth2DemoResource {
     }
 
     @GetMapping("/login")
-    public ModelAndView getSsoPage() {
-        return new ModelAndView("login");
+    public String getSsoPage(@RequestParam(value = "type", required = false) String loginType, Model model) {
+        if (loginType == null) {
+            return "redirect:/login?type=mock";
+        }
+
+        boolean real = loginType.equals("real");
+        String toggleType = real ? "mock" : "real";
+        String loginProvider = real ? "google" : "mock-oidc";
+
+        model.addAttribute("loginType", loginType)
+             .addAttribute("toggleType", toggleType)
+             .addAttribute("loginProvider", loginProvider)
+             .addAttribute("real", real);
+
+        return "login";
     }
 }
